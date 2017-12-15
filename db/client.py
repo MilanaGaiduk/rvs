7e2f7edeee7ef7781998a6e772ed392b2249d154
MilanaGaiduk <milagaiduk@gmail.com>
2017-12-15 13:15:43 +0300

    with processing on client

--- db/client.py
+++ db/client.py
@@ -3,15 +3,28 @@ import socket
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_sock.connect(('192.168.249.130', 53210))
 
+f1 = open('cldb.txt','r')
+
 a=''
+flag=True
 print('Write STOP to stop interacting')
 while a!= 'STOP':
     print("Write id")
     a = input()
     client_sock.sendall(a.encode())
     data = client_sock.recv(1024)
-    term = data.decode()
-    print('Received: ',term)
+    name = data.decode()
+    print('Received: ',name)
+    if name=='Error! This id does not exist in database':
+        flag=False
+    with open('cldb.txt') as f1:
+            for line in f1:
+                if line[0]==a:
+                    answer = line[2:]
+    if flag:
+        print(name+" works on project "+ answer)
+    flag=True
+	
 client_sock.close()
 print('Connection closed')
 
